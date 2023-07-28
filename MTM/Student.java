1359c40e2bcd280c2a07282bfddb505b577256e2
chinna63055 <120019918+chinna63055@users.noreply.github.com>
2023-07-28 17:32:30 +0530

    Add files via upload

---
+++ MTM/Student.java
@@ -0,0 +1,36 @@
+package MTM;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Student {
+	@Id
+	private int sid;
+	private String name;
+	@ManyToMany
+	private  List<Course> courses;
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+}
