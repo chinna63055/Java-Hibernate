ec1793d2ed54218e7ea3b734c82b87ca8563b257
chinna63055 <120019918+chinna63055@users.noreply.github.com>
2023-07-30 23:26:32 +0530

    Add files via upload

---
+++ hibernate-OTO/src/main/java/STATE/SaveState.java
@@ -0,0 +1,22 @@
+package STATE;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class SaveState {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("vikas");
+		EntityManager em= emf.createEntityManager();
+		EntityTransaction et=em.getTransaction();
+		
+		State state1 = new State();
+		state1.setNo(1);
+		state1.setName("Midin Choudharri");
+		state1.setCapital("Kadapa");
+		
+	}
+
+}
