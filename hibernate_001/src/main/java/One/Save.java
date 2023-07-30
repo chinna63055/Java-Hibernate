a3ddcacbff258e1a7489732a1e05df9c670a565b
chinna63055 <120019918+chinna63055@users.noreply.github.com>
2023-07-30 23:24:28 +0530

    Add files via upload

--- hibernate_001/src/main/java/One/Save.java
+++ hibernate_001/src/main/java/One/Save.java
@@ -1,20 +1,21 @@
-package EmployeeProject;
+package One;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class DeleteEmmployee {
+public class Save {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("vikas");
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("vikas");
 		EntityManager em=emf.createEntityManager();
 		EntityTransaction et=em.getTransaction();
+		T1 t = new T1();
+		t.setId(23);
 		
-		Employee e=new Employee();
-		
-		
+		et.begin();
+		em.persist(t);
+		et.commit();
 	}
-
 }
